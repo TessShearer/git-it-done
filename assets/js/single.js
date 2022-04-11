b4bd6ca2bed849f7798a834555358121a92db3a7
TessShearer <shearer.tess@gmail.com>
2022-04-11 11:31:15 -0600

    adapted for more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
-
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -15,11 +15,23 @@ var getRepoIssues = function(repo) {
         else {
           alert("There was a problem with your request!");
         }
+
+            // check if api has paginated issues
+    if (response.headers.get("Link")) {
+        displayWarning(repo);
+      }
+
       });
 };
   
 
 var displayIssues = function(issues) {
+
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+      }
+
     for (var i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
         var issueEl = document.createElement("a");
@@ -50,5 +62,18 @@ if (issues[i].pull_request) {
 issueEl.appendChild(typeEl);
 };
 
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+
+  };
 
   getRepoIssues("facebook/react");
